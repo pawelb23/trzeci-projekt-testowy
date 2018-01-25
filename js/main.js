4a6f263540f72d3cd59d542c103e7c6c9894557b
Paweł Białek <pawelbialek23@op.pl>
2018-01-25 06:22:39 +0100

    takie tam

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 alert("Alarm!")
 console.log("Hello World!")
+var myHeading=document.querySelector('h1');
+myHeading.textContent='Hello World';
+var name="Jan Nowak"
+console.log(name)
+var num1=2;
+var num2=3;
+function multiply(num1,num2){
+var result=num1*num2;
+return result;
+}
+document.querySelector('h1').onclick=function(){
+    alert('Nie wciskaj byle czego');
+}
